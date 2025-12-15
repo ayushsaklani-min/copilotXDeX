406127562742353113cc83682307c4442cb4d42b
ayush saklani <saklaniayush97@gmail.com>
2025-12-15 23:56:24 +0530

    Security fix: Update Next.js to 16.0.10 to fix CVE-2025-66478 vulnerability

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
   env: {
     NEXT_PUBLIC_ALCHEMY_API_KEY: "9BF8nSZfP4AubPhp5y8AX",
   },
